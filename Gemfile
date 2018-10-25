1b3e37c4b68d5087c2d0ff10ab71832bf9072cfc
James Duong <james.duong93@gmail.com>
2018-10-25 17:02:35 +1100

    imageupload

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise", "~> 4.5"
+
+gem "better_errors"
+gem "binding_of_caller"
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,6 @@
 class ProfileController < ApplicationController
 
     def index
-
+        @avatar = "https://thediplomat.com/wp-content/uploads/2017/10/thediplomat-ap_923638204453-386x261.jpg"
     end
 end
